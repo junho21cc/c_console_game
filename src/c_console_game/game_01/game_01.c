c6f61283b0852b22638afe094b998ef995293541
skshin <skshin@gmail.com>
2019-12-10 16:51:23 +0900

    기본 게임 루프 구현 및 키보드 입력 테스트

--- src/c_console_game/game_01/game_01.c
+++ src/c_console_game/game_01/game_01.c
@@ -1,6 +1,41 @@
-﻿#include "cursor.h"
+﻿#include <stdio.h>
+#include <conio.h>
+#include "cursor.h"
+
+#define putsxy(x, y, s) {gotoxy(x, y);puts(s);}
+#define KEY_ESC 27
 
 int main()
 {
+	clrscr();
+	putsxy(45, 2, "KEY TEST");
+	putsxy(45, 4, "ESC:종료");
+
+	// 게임 진행 루프
+	for (;;)
+	{
+		int ch;
+		ch = _getch();
+		printf("[1]key = %d, 0x%X\n", ch, ch);
+		if (ch == 0xE0 || ch == 0)
+		{
+			ch = _getch();
+			printf("[2]key = %d, 0x%X\n", ch, ch);
+			/*
+			switch (ch)
+			{
+				case 
+			}
+			*/
+		}
+		else
+		{
+			if (ch == KEY_ESC)
+			{
+				break;
+			}
+		}
+	}
+
 	return 0;
 }
