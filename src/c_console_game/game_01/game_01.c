c66506b0836fe594fef5f20c322d20bf66f809df
신준호 <junho1018@gmail.com>
2019-12-18 21:29:30 +0900

    A문자를 방향키를 입력받아서 움직이게 하였다.( #1 )

--- src/c_console_game/game_01/game_01.c
+++ src/c_console_game/game_01/game_01.c
@@ -12,22 +12,54 @@ int main()
 	clrscr();
 	putsxy(45, 2, "KEY TEST");
 	putsxy(45, 4, "ESC:종료");
+	putsxy(55, 10, "A");
 
 	// 게임 진행 루프
+	int x = 55;
+	int y = 10;
+
 	for (;;)
 	{
-		int ch;
+		int ch;	
 		ch = getch();
-		printf("[1]key = %d, 0x%X\n", ch, ch);
 		if (ch == 0xE0 || ch == 0)
 		{
 			ch = getch();
-			printf("[2]key = %d, 0x%X\n", ch, ch);
-			/*
-			switch (ch)
+			
+			if (ch == 72)
 			{
-				case 
+				putsxy(x, y - 1, "A");
+				putsxy(x, y, " ");
+				y = y - 1;
+				printf("(%d, %d)", x, y);
 			}
+			else if(ch == 80)
+			{
+				putsxy(x, y + 1, "A");
+				putsxy(x, y, " ");
+				y = y + 1;
+				printf("(%d, %d)", x, y);
+			}
+			else if (ch == 75)
+			{
+				putsxy(x - 1, y, "A");
+				putsxy(x, y, " ");
+				x = x - 1;
+				printf("(%d, %d)", x, y);
+			}
+			else
+			{
+				putsxy(x + 1, y, "A");
+				putsxy(x, y, " ");
+				x = x + 1;
+				printf("(%d, %d)", x, y);
+			}
+			/*
+			if (x < 0)
+			{
+				x = 0;
+			}
+			if(x > 118 || y > 28)
 			*/
 		}
 		else
