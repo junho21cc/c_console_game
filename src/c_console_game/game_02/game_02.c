eea1d0349291557cc06f9d774a5ab429e37c1ccf
신준호 <junho1018@gmail.com>
2020-01-07 00:06:56 +0900

    테트리스 맵 만들기 : #8

---
+++ src/c_console_game/game_02/game_02.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#if !defined(__APPLE__)
+#include <conio.h>
+#endif
+#include "cursor.h"
+
+#define putsxy(x, y, s) {gotoxy(x, y);puts(s);}
+#define KEY_ESC 27
+
+char tetris[21][11] = {
+	"#        #",
+	"#        #",
+	"#        #",
+	"#        #",
+	"#        #",
+	"#        #",
+	"#        #",
+	"#        #",
+	"#        #",
+	"#        #",
+	"#        #",
+	"#        #",
+	"#        #",
+	"#        #",
+	"#        #",
+	"#        #",
+	"#        #",
+	"#        #",
+	"#        #",
+	"##########",
+};
+
+int main()
+{
+	for (int i = 0; i < 20; i++)
+	{
+		printf("%s\n", tetris[i]);
+	}
+	return 0;
+}
