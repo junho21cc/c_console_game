063ee43509e14208682a0bad3ca2531e90129889
신준호 <junho1018@gmail.com>
2020-01-08 22:23:40 +0900

    블록 모양 7개 추가함 : #9

--- src/c_console_game/game_02/game_02.c
+++ src/c_console_game/game_02/game_02.c
@@ -30,11 +30,75 @@ char tetris[21][11] = {
 	"##########",
 };
 
+char tetris_block[10][5][5] =
+{
+	{ // ㅣ형
+		"@",
+		"@",
+		"@",
+		"@"
+	},
+	{ // ㅁ형
+		"@@",
+		"@@"
+	},
+	{ //L 형
+		"@",
+		"@",
+		"@@"
+	},
+	{ // ㅗ형
+		"@",
+		"@@",
+		"@"
+	},
+	{ // ㄱ형
+		"@@@",
+		"@"
+	},
+	{ // Z형
+		"@@",
+		" @@"
+	},
+	{ // S형
+		" @@",
+		"@@"
+	}
+};
+
 int main()
 {
+	gotoxy(0, 4);
 	for (int i = 0; i < 20; i++)
 	{
-		printf("%s\n", tetris[i]);
+		// 띄어쓰기 10번임
+		printf("          %s\n", tetris[i]);
 	}
+
+	int random = rand() % 7;
+	int ch;
+
+	ch = getch();
+	if (ch == 0xE0 || ch == 0)
+	{
+		ch = getch();
+
+		for (int i = 0; i < 5; i++)
+		{
+			gotoxy(13, 4 + i);
+			printf("%s\n", tetris_block[random][i]);
+
+		}
+	}
+
+	for (;;)
+	{
+		for (int i = 0; i < 15; i++)
+		{
+			gotoxy(14, 8 + i);
+			printf("%s\n", tetris_block[random][i]);
+		}
+	}
+
 	return 0;
 }
